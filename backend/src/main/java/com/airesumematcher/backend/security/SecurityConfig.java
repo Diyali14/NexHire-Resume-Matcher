6cc2a085bf103d52a1f3a1154ea2e1f11290f64c
Diyali14 <diyali14082004@gmail.com>
2026-09-22 11:25:54 +0530

    Added cors

--- backend/src/main/java/com/airesumematcher/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/airesumematcher/backend/security/SecurityConfig.java
@@ -96,34 +96,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                        "http://localhost:5500",
-                        
-                        "http://localhost:3000",
-                        "http://localhost:5000",
-                        "http://localhost:5001",
-                        "http://localhost:5501",
-                        "http://127.0.0.1:5500",
+
                         "http://127.0.0.1:5501",
                         "http://127.0.0.1:5502",
-                        "http://127.0.0.1:5503",
-                        "http://127.0.0.1:5504",
-                        "http://127.0.0.1:5505",
-                        "http://127.0.0.1:5506",
-                        "http://127.0.0.1:5507",
-                        "http://127.0.0.1:5508",
-                        "http://127.0.0.1:5509",
-                        "http://127.0.0.1:5510",
-                        "http://localhost:5500",
-                        "http://localhost:5501",
-                        "http://localhost:5502",
-                        "http://localhost:5503",
-                        "http://localhost:5504",
-                        "http://localhost:5505",
-                        "http://localhost:5506",
-                        "http://localhost:5507",
-                        "http://localhost:5508",
-                        "http://localhost:5509",
-                        "http://localhost:5510",
+                        
                         "https://nex-hire-11i2.vercel.app",
 			"https://nex-hire-frontend.vercel.app",
 			"https://nex-hire-resume-matcher.vercel.app"));
